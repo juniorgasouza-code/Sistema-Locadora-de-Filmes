ee48af61f3a77dbd4ed9bdd48e5c6dc82c3e51f4
juniorgasouza-code <junior_ga_souza@estudante.sesisenai.org.br>
2025-08-07 10:55:42 -0300

    Create SIstemaLocadoraFilmes.java
    Criação de 3 metodos

---
+++ SIstemaLocadoraFilmes.java
@@ -0,0 +1,85 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SIstemaLocadoraFilmes {
+    static Scanner SC = new Scanner(System.in);
+    static HashMap<String, String> filmes = new HashMap<>();
+    static HashMap<String, String> clientes = new HashMap<>();
+
+    public static void main(String[] args) {
+    }
+
+    public static void mostrarMenu() {
+        while (true) {
+            System.out.println(
+                    "1-Cadastrar Filme\n2-Cadastrar Cliente\n3-Realizar Operações\n4-Listar locações de um cliente\n5-Sair\n>");
+            int escolhaMostrarMenu = SC.nextInt();
+            switch (escolhaMostrarMenu) {
+                case 1:
+                    // Cadastrar filmes
+                    cadastrarfilmes();
+                    break;
+                case 2:
+                    // Cadastro Clientes
+                    cadastroClientes();
+                    break;
+                case 3:
+                    // Realizar Operção
+                    realizarOperacoes();
+                    break;
+                case 4:
+                    listarLocacoesCliente();
+                    break;
+                case 5:
+                    System.out.println("Saindo do sistema!");
+                    System.exit(0);
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void cadastrarfilmes() {
+        System.out.println("Digite o codigo do filme\n>");
+        String codigo = SC.next();
+        if (filmes.containsKey(codigo)) {
+            System.out.println("Filme ja cadastrado! voltando ao menu principal...");
+            return;
+        } else {
+            System.out.println("Insira o nome do filme\n>");
+            String nome = SC.next();
+            filmes.put(codigo, nome);
+            System.out.println("Cadastro de filme realizado com sucesso!");
+        }
+    }
+
+    public static void cadastroClientes() {
+        System.out.println("Insira o cpf do cliente\n>");
+        String cpf = SC.next();
+        if (clientes.containsKey(cpf)) {
+            System.out.println("Cliente ja cadastrado! voltando ao menu principal");
+            return;
+        } else {
+            System.out.println("Insira o nome do cliente\n>");
+            String nome = SC.next();
+            clientes.put(cpf, nome);
+            System.out.println("Cadastro de cliente realizado com sucesso!");
+        }
+    }
+
+    public static void realizarOperacoes(){
+        System.out.println("Informe seu cpf\n>");
+        String cpf = SC.next();
+        if(!clientes.containsKey(cpf)){
+            System.out.println("CPF não cadastrado!");
+        }else{
+            System.out.println("Filmes disponiveis");
+            for (String  codigos : filmes.keySet()) {
+                String titulos = filmes.get(codigos);
+                System.out.println("Codigo: " + codigos + " Titulos: " + titulos);
+            }
+                
+            }
+        }
+    }
+
